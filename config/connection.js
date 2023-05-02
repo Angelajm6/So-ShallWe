cce833e8f497131e2bdb40eff17975a003e38594
angelajm6 <angelajaume.m@gmail.com>
2023-05-02 17:11:41 -0400

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,8 @@
 const { connect, connection } = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+connect('mongodb://localhost/SocialNetworkAPI', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// Below will log mongo queries being executed
-mongoose.set("debug", true);
-
-module.exports = mongoose.connection;
+module.exports = connection;
